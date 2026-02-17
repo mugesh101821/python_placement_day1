860904fcea4c8b8999135796a6a538e57b2c6871
mugesh101821 <saravananwm01@gmail.com>
2026-02-17 09:57:01 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,101 @@
+# DATA TYPE 
+# four types:int,float,str,bool.
+
+
+# birth_year=2006
+# future_year=2026
+# print(future_year-birth_year)
+
+
+# current_age=20
+# maximum_age=40
+# estimated_number=100
+# one_month=100*30
+# one_year=one_month*12
+# years_live=maximum_age-current_age
+# total_expence=years_live*one_year
+# print("you will need",total_expence,"to live")
+
+
+# current_age=20
+# maximum_age=40
+# water_per_day=4
+# years_left=maximum_age-current_age
+# no_of_days=years_left*365
+# total_water_consumption=water_per_day*no_of_days
+# print("you will drink",total_water_consumption,"litres of water by age",maximum_age)
+
+
+# total_trip_distance=100
+# car_mileage=25
+# petrol_price=110
+# fuel_needed=total_trip_distance//car_mileage
+# total_cost=fuel_needed*petrol_price
+# print("total fuel cost for the trip is $",total_cost)
+
+
+# daily_data_usage=2
+# no_of_days_in_month=30
+# monthly_data_usage=daily_data_usage*no_of_days_in_month
+# print("you use",monthly_data_usage,"GB of data per month")
+
+
+
+# current_age=20
+# no_of_days_lived=20*365
+# print("you are approximately",no_of_days_lived,"days old")
+
+
+# original_price=2500
+# discount_percentage=25
+# discount_price=(discount_percentage*original_price)//100
+# final_price=original_price-discount_price
+# print("final price after discout is",final_price)
+
+
+# x=int(input("enter number:"))
+# y=int(input("enter number:"))
+# if x<y:
+#     print(y,"is greater than",x)
+# else:
+#     print(x,"is greater than",y)
+
+
+# char=input("enter a character:")
+# if char=='a' or char=='e' or char=='i' or char=='o' or char=='u':
+#     print("it is a vowel")
+# else:
+#     print("it is a consonent")
+
+
+
+# x=int(input("enter a student mark="))
+# if x>100:
+#     print("INVALID MARK")
+# elif X==100:
+#     print("A")
+# elif x>89:
+#     print("A")
+# elif x>79:
+#     print("B")
+# elif x>69:
+#     print("C")
+# elif x>59:
+#     print("D")
+# elif x>49:
+#     print("E")
+# else:
+#     print("F")
+
+
+
+# x=float(input("enter banana doxen rate:"))
+# y=float(input("one banana selling amount:"))
+# one_dozen=12
+# expected_amount_for_one_banana=x/one_dozen
+# l=expected_amount_for_one_banana-y
+# p=y-expected_amount_for_one_banana
+# if expected_amount_for_one_banana>y:
+#     print("LOSS=",l)
+# else:
+#     print("PROFIT=",p)
